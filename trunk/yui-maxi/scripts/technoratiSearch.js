f0c17948ca19692dc96763d48189331e5025f670
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-01-30 19:19:41 +0000

    Technorati blog search script.

---
+++ trunk/yui-maxi/scripts/technoratiSearch.js
@@ -0,0 +1,39 @@
+  function requestTechnoratiData(movie){
+  
+  	  var title = movie.title;
+  	  
+      function successHandler(o) {
+
+          var resultArray = new Array();
+          var root = o.responseXML.documentElement;
+          var titles = root.getElementsByTagName('item');
+
+          for(i=0; i<titles.length; i++){
+	      var date = titles[i].getElementsByTagName('date')[0].firstChild.nodeValue;
+	      var count = titles[i].getElementsByTagName('count')[0].firstChild.nodeValue;
+			resultArray.push({"date":date,"count":parseInt(count)});
+          }
+          
+          if(movie.technoratiData === undefined){
+            movie.technoratiData = {};
+          }
+		
+		  movie.technoratiData.postsByDate = resultArray;
+		
+      }
+
+      function failureHandler(o) {
+      }
+
+      // A variable for the AWS account on which the requests will be made.
+      // Your AWS ID as a string.
+      var apiKey = '1c448f36f2564d9a126ae581078b818e';
+      // The URL for the asynchronous request, make sure this points to
+      // your own web proxy!
+      var sUrl = 'TechnoratiProxy.cgi?http://api.technorati.com/dailycounts?key=' +
+                  apiKey + '&q=' + title;
+
+
+      var request = YAHOO.util.Connect.asyncRequest('GET', sUrl,
+           {success:successHandler, failure:failureHandler});
+  }
