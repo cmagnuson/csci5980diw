12cfafec95a43247ba61f5943c6578ead31fbbbc
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-01-30 20:21:39 +0000

    Review datatable working, needs come cleaning up.

--- trunk/yui-maxi/scripts/display.js
+++ trunk/yui-maxi/scripts/display.js
@@ -39,3 +39,29 @@
    
    }
  }
+ 
+ function showReviewTable(movie, div) { 
+ 		if(movie.awsData === undefined){
+		 }
+	      else{  var myColumnDefs = [ 
+	            {key:"rating", sortable:true, resizeable:true}, 
+	            {key:"date", formatter:YAHOO.widget.DataTable.formatDate, sortable:true, sortOptions:{defaultDir:YAHOO.widget.DataTable.CLASS_DESC},resizeable:true}, 
+	            {key:"votes", formatter:YAHOO.widget.DataTable.formatNumber, sortable:true, resizeable:true}, 
+	            {key:"content", sortable:true, resizeable:true} 
+	        ]; 
+	 
+	        var myDataSource = new YAHOO.util.DataSource(movie.awsData.reviews); 
+	        myDataSource.responseType = YAHOO.util.DataSource.TYPE_JSARRAY; 
+	        myDataSource.responseSchema = { 
+	            fields: ["rating","date","votes","content"] 
+	        }; 
+	 
+	        var myDataTable = new YAHOO.widget.DataTable(div, 
+	                myColumnDefs, myDataSource, {caption:"DataTable Caption"}); 
+	                 
+	        return { 
+	            oDS: myDataSource, 
+	            oDT: myDataTable 
+	        }; 
+	        }
+	    }
