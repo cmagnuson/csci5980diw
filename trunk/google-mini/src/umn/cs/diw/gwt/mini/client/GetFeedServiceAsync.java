b4e7cda5e1f2185df2f2847c103fac601179663e
ande7966 <ande7966@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-02-18 18:45:03 +0000

    Defined getFeedItems as the first step of the tutorial. (Also testing subclipse).

--- trunk/google-mini/src/umn/cs/diw/gwt/mini/client/GetFeedServiceAsync.java
+++ trunk/google-mini/src/umn/cs/diw/gwt/mini/client/GetFeedServiceAsync.java
@@ -4,4 +4,5 @@ import umn.cs.diw.gwt.mini.client.datamodel.*;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface GetFeedServiceAsync {
+	void getFeedItems(String tag, AsyncCallback sync);
 }	
