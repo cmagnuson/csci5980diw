cd81c702af9bda3c1526a38fdfdf7f7938e44e39
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-02-21 22:47:30 +0000

    Added shell classes for getting previous searches.

---
+++ google-maxi/src/com/l33tsp33k/server/GetCachedSearches.java
@@ -0,0 +1,21 @@
+package com.l33tsp33k.server;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class for Servlet: GetCachedSearches
+ *
+ */
+ public class GetCachedSearches extends com.google.gwt.user.server.rpc.RemoteServiceServlet implements javax.servlet.Servlet {
+   static final long serialVersionUID = 1L;
+   
+    /* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public GetCachedSearches() {
+		super();
+	}   	 	  	  	    
+}
