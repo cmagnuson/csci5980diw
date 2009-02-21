cd9ef3b61edeec9bcac073ac9ce927ba9703612c
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-02-21 22:53:42 +0000

    Cleaned things up, removed some compile warnings.

--- google-maxi/src/com/l33tsp33k/server/GetFlickrData.java
+++ google-maxi/src/com/l33tsp33k/server/GetFlickrData.java
@@ -1,11 +1,5 @@
 package com.l33tsp33k.server;
 
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 /**
  * Servlet implementation class for Servlet: GetFlickrData
  *
