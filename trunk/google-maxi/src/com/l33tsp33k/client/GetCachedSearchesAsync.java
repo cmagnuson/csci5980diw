f4896130a1b294e3d7bf3f9472d0e4ffd0c4b059
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-02-22 01:22:44 +0000

    Added code to pull cached searches from DB, populate oracle with those searches.  May not work on campus in hosted mode due to firewall restrictions???

---
+++ trunk/google-maxi/src/com/l33tsp33k/client/GetCachedSearchesAsync.java
@@ -0,0 +1,10 @@
+package com.l33tsp33k.client;
+
+import com.l33tsp33k.client.datamodels.*;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface GetCachedSearchesAsync {
+
+	void getCachedSearches(AsyncCallback<CachedSearchList> callback);
+
+}
