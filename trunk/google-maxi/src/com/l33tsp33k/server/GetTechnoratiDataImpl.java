abcd2ae5c33dfd340f7027375af5774caf6524b7
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-02-24 02:23:56 +0000

    Fix for null pointer error in Technorati data source.

--- trunk/google-maxi/src/com/l33tsp33k/server/GetTechnoratiDataImpl.java
+++ trunk/google-maxi/src/com/l33tsp33k/server/GetTechnoratiDataImpl.java
@@ -13,38 +13,41 @@ import com.sun.syndication.io.XmlReader;
 
 
 public class GetTechnoratiDataImpl extends RemoteServiceServlet implements GetTechnoratiData {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	// we'll use this url in a bit for our tag syndication service
 	private final String feedProviderUrl = "http://feeds.technorati.com/tag/";
-	
+
 	// append this string to the end of a technorati query for better results	
 	@SuppressWarnings("unused")
 	private final String feedQuery = "?authority=a4&language=en";
-	
+
 	@SuppressWarnings("unchecked")
 	public TechnoratiItemList getFeedItems(String tag) {
-	    TechnoratiItemList list = new TechnoratiItemList();
-	    try{
-	    	  String feedURI = feedProviderUrl + tag;
-	    	  SyndFeedInput input = new SyndFeedInput();
-	    	  SyndFeed feed = input.build(new XmlReader(new URL(feedURI)));
-	    		    
-	    	  // Iterate through feed items
-	    	  
-	    	  Iterator entryIter = feed.getEntries().iterator();
-	    	  while (entryIter.hasNext()) {
-	    	    SyndEntry entry = (SyndEntry) entryIter.next();
-	    	    String title = entry.getTitle();
-	    	    String uri = entry.getUri();
-	    	    String author = entry.getAuthor();
-	    	    String desc = entry.getDescription().getValue();
-	    	    list.addFeedItem(title, uri, author, desc);
-	    	  }
-	    	} catch (Exception e) {
-	    	      	e.printStackTrace(); 
-	    	}	    
-	    return list;
+		TechnoratiItemList list = new TechnoratiItemList();
+		try{
+			String feedURI = feedProviderUrl + tag;
+			SyndFeedInput input = new SyndFeedInput();
+			SyndFeed feed = input.build(new XmlReader(new URL(feedURI)));
+
+			// Iterate through feed items
+
+			Iterator entryIter = feed.getEntries().iterator();
+			while (entryIter.hasNext()) {
+				SyndEntry entry = (SyndEntry) entryIter.next();
+				String title = entry.getTitle();
+				String uri = entry.getUri();
+				String author = entry.getAuthor();
+				String desc = "";
+				if(entry.getDescription()!=null){
+					entry.getDescription().getValue();
+				}
+				list.addFeedItem(title, uri, author, desc);
+			}
+		} catch (Exception e) {
+			e.printStackTrace(); 
+		}	    
+		return list;
 	}
 }
