156641302718d38d68827af437c440e65a1464db
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-02-27 02:50:13 +0000

    Adding geocoding classes.

---
+++ trunk/google-maxi/src/com/l33tsp33k/server/AddressToCord.java
@@ -0,0 +1,34 @@
+package com.l33tsp33k.server;
+
+import java.net.*;
+import java.io.*;
+
+public class AddressToCord {
+	
+	final static String googleId = "ABQIAAAAiuPVg850W_6nz_Xq0YZbBhTMfOSgnTFSBJHlxt2tIHyOtUx0YRQHMJIX6DCPqOg61WX8TQwjzYRj1A";
+	final static String dataType = "csv";
+	
+	@SuppressWarnings("deprecation")
+	public static Location convertToCord(String address){
+		Location ret = null;;
+		String input = null;
+		try{
+			String htmlAddress = URLEncoder.encode(address);
+			URL u2 = new URL("http://maps.google.com/maps/geo?q="+htmlAddress+"&output="+dataType+"&key="+googleId);
+			HttpURLConnection u = (HttpURLConnection)u2.openConnection();
+			u.addRequestProperty("User-Agent","Mozilla/4.76");
+
+			BufferedReader br;
+			try{br = new BufferedReader(new InputStreamReader(u.getInputStream()));}
+			catch(IOException e){
+				return null;
+			}
+			input = br.readLine();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		ret = new Location(Integer.valueOf(input.split(",")[0]),Double.valueOf(input.split(",")[1]),Double.valueOf(input.split(",")[2]),Double.valueOf(input.split(",")[3]));
+		return ret;
+	}
+}
