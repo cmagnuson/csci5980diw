4d894f5c941afd683f647ffb789a1ef9fd1c7991
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-03-04 22:52:23 +0000

    Added geocoding for twitter data, displaying on map.

--- trunk/google-maxi/src/com/l33tsp33k/client/datamodels/TwitterItem.java
+++ trunk/google-maxi/src/com/l33tsp33k/client/datamodels/TwitterItem.java
@@ -2,15 +2,50 @@ package com.l33tsp33k.client.datamodels;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
-public class TwitterItem implements IsSerializable, java.io.Serializable {
-	/**
-	 * 
-	 */
+public class TwitterItem implements IsSerializable, java.io.Serializable, GeoReferencedData {
+
 	private static final long serialVersionUID = 6992873494030300174L;
 	public String name = "";
 	public String link = "";
 	public String author = "";
 	public String descr = "";
+	private boolean coordinatesAssigned = false;
+	private boolean locationAssigned = false;
+	private String location = "";
+	private double lat = 0;
+	private double lon = 0;
+
+	public boolean hasCoordinates() {
+		return coordinatesAssigned;
+	}
+
+
+	public boolean hasLocation() {
+		return locationAssigned;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+		locationAssigned = true;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public double getLong() {
+		return lon;
+	}
+
+	public void setCoords(double lon, double lat) {
+		this.lon = lon;
+		this.lat = lat;
+		coordinatesAssigned = true;
+	}
 
 	public TwitterItem() {}
 
