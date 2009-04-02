ee7fd7e20957181ef0d81dabc8f7a4553b2f5a06
bjecky <bjecky@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-04-02 18:06:55 +0000

    Added a very basic layout for our project.

---
+++ sl-maxi/SilverlightMaxi/SilverlightMaxi/Page.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightMaxi
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        private void emailSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            runSearch();
+        }
+
+        private void emailSearchTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Enter))
+                runSearch();
+        }
+
+        private void runSearch()
+        {
+            // Check if email adress is in system
+            // If so, generate graphs
+            // Else, generate a message on how to get added to the system
+        }
+    }
+}
