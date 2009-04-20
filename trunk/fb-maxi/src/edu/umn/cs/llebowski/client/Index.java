07cc4a5b6abf8b04744d474f327ebc8a8cd1f395
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-04-20 17:29:09 +0000

    Initial import.

---
+++ trunk/fb-maxi/src/edu/umn/cs/llebowski/client/Index.java
@@ -0,0 +1,11 @@
+package edu.umn.cs.llebowski.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Index implements EntryPoint {
+
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub 
+	}
+
+}
---
+++ trunk/fb-maxi/src/edu/umn/cs/llebowski/client/dataviews/MapPanel.java
@@ -0,0 +1,124 @@
+package edu.umn.cs.llebowski.client.dataviews;
+
+import java.util.LinkedList;
+
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.maps.client.*;
+import com.google.gwt.maps.client.control.*;
+import com.google.gwt.maps.client.event.MarkerClickHandler;
+import com.google.gwt.maps.client.overlay.MarkerOptions;
+import com.google.gwt.maps.client.overlay.Polyline;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.maps.client.overlay.Marker;
+import com.google.gwt.maps.client.geom.LatLng;
+
+import edu.umn.cs.llebowski.client.datamodels.*;
+
+public class MapPanel extends SimplePanel {
+
+	private final MapWidget mapWidget = new MapWidget();
+	public AbsolutePanel absolutePanel = new AbsolutePanel();
+	
+	private static final int REFRESH_RATE = 15*1000;
+	private static final String LINE_COLOR = "#52FF52";
+	private static final int LINE_WIDTH = 4;
+	private static final double LINE_OPACITY = 1;
+	
+	public MapPanel(){
+		super();
+		final MapPanel mp = this;
+		this.setTitle("Friend Map");
+		this.setSize("100%", "500px");
+
+		final AbsolutePanel ap = absolutePanel;
+		ap.setSize("100%", "100%");
+		absolutePanel.add(mapWidget,0,0);
+		mapWidget.setSize("100%", "100%");
+		this.add(ap);
+
+		//set zoom based off of some user presets....
+		//mapWidget.setCenter(LatLng.newInstance(map.getCenter_lat(), map.getCenter_lon()), map.getZoom());  
+
+		mapWidget.addControl(new SmallMapControl(), new ControlPosition(ControlAnchor.TOP_RIGHT, 10, 10));  
+		mapWidget.checkResizeAndCenter();	
+
+		mp.scheduleTimer();	
+	}
+
+
+	private void scheduleTimer(){
+		Timer t = new Timer() {
+			public void run() {
+				refreshMap();
+			}
+		};
+		t.scheduleRepeating(REFRESH_RATE);
+	}
+
+
+
+	public void onLoad(){
+		super.onLoad();
+		checkResizeAndCenter();	
+	}
+
+	public void onAttach(){
+		super.onAttach();
+		checkResizeAndCenter();	
+	}
+
+	public void refreshMap(){
+		//LinkedList<Split> splits = mtrack.getSplits(runnerPid);
+//		if(splits==null){
+//		splits = new LinkedList<Split>();
+//		}
+		drawMap();
+	}
+
+	public void checkResizeAndCenter(){
+		if(mapWidget!=null){
+			mapWidget.checkResizeAndCenter();
+		}
+	}
+
+	private void drawMap(){
+		mapWidget.clearOverlays(); //or just check which ones need to be redrawn
+
+		//do drawing stuff
+	}
+
+	public void addPersonPoint(Person p, Location l){
+		String infoText = "";
+		MarkerOptions options = MarkerOptions.newInstance();
+		options.setTitle(infoText.replaceAll("<br>", ""));
+
+		final Marker sm = new Marker(LatLng.newInstance(l.getLat(),l.getLon()), options);
+		mapWidget.addOverlay(sm);
+
+		String imageUrl = p.getProfilePic();
+		sm.setImage(imageUrl);
+
+		final String infoTextf = infoText;
+
+		sm.addMarkerClickHandler(new MarkerClickHandler() {
+			public void	onClick(MarkerClickHandler.MarkerClickEvent event){
+				InfoWindow iw = mapWidget.getInfoWindow();
+				iw.open(sm, new InfoWindowContent(infoTextf));
+			}
+		});
+	}
+
+	public void addPersonTrack(Person p){
+		LinkedList<LatLng> points = new LinkedList<LatLng>();
+
+		for(Location l: p.getLocations()){
+			points.add(LatLng.newInstance(l.getLat(), l.getLon()));
+			addPersonPoint(p,l);
+		}
+
+		Polyline line = new Polyline(points.toArray(new LatLng[]{}), LINE_COLOR, LINE_WIDTH, LINE_OPACITY);
+		mapWidget.addOverlay(line);
+	}
+
+}
