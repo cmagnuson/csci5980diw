af313574e58d98d8b9af396f65132968bda8c3ed
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-04-22 21:19:00 +0000

    Added some useful libraries and utility files.

---
+++ trunk/fb-maxi/src/edu/umn/cs/llebowski/server/Location.java
@@ -0,0 +1,56 @@
+package edu.umn.cs.llebowski.server;
+
+public class Location {
+
+	double lon = 0;
+	double lat = 0;
+	double accuracy = 0;
+	int statusCode = 0;
+	
+	Location(){}
+	
+	Location(int statusCode, double accuracy, double lat, double  lon){
+		this.statusCode = statusCode;
+		this.accuracy = accuracy;
+		this.lat = lat;
+		this.lon = lon;
+	}
+	
+	public String toString(){
+		return ""+statusCode+", "+accuracy+", "+lat+", "+lon;
+	}
+
+	public double getAccuracy() {
+		return accuracy;
+	}
+
+	public void setAccuracy(double accuracy) {
+		this.accuracy = accuracy;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	
+	
+}
