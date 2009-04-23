5935cbebccbd7ccea822b4d0877d16f9e9a3d3a6
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-04-23 18:55:17 +0000

    Stub view for sidepanel.

---
+++ fb-maxi/src/edu/umn/cs/llebowski/client/dataviews/SidePanel.java
@@ -0,0 +1,11 @@
+package edu.umn.cs.llebowski.client.dataviews;
+
+import com.google.gwt.user.client.ui.SimplePanel;
+
+public class SidePanel extends SimplePanel {
+
+	SidePanel(){
+		super();
+		
+	}
+}
