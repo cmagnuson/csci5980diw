b9e10cb40e79c3eaecb38fbdc497167f61a0209e
magnuson.carl <magnuson.carl@6d9ae19a-ea4d-11dd-b00f-9fabb4b2ff13>
2009-04-24 23:05:24 +0000

    Ugly login code, gets sessionid and uid from cookie and creates credentials.  Login.html can be prettied up, and can check if cookies already exist to redirect to index automatically if no login required.

--- trunk/fb-maxi/src/edu/umn/cs/llebowski/client/Index.java
+++ trunk/fb-maxi/src/edu/umn/cs/llebowski/client/Index.java
@@ -1,11 +1,31 @@
 package edu.umn.cs.llebowski.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import edu.umn.cs.llebowski.client.datamodels.*;
+import com.google.gwt.user.client.ui.*;
 
 public class Index implements EntryPoint {
 
+	public static final String API_KEY = "d4dd44d81ce3c4076e4c55ae2329f13d";
+	public static final String SECRET_KEY = "4803662fcf9e1bdfdc8c29d2adfe2bdd";
+
+	private long uid = 1;
+	private String sessionKey = "";
+	private FacebookCredentials credentials = new FacebookCredentials();
+
+	
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub 
+		getCredentials();
+		RootPanel.get().add(new HTML(credentials.toString()));
+	}
+	
+	private void getCredentials(){
+		uid = Long.valueOf(com.google.gwt.user.client.Cookies.getCookie(API_KEY+"_user"));
+		sessionKey = com.google.gwt.user.client.Cookies.getCookie(API_KEY+"_session_key");
+		credentials.setApiKey(API_KEY);
+		credentials.setSecretKey(SECRET_KEY);
+		credentials.setSessionId(sessionKey);
+		credentials.setUid(uid);
 	}
 
 }
